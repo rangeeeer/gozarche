d0dd355df268d9437211835aff45f78b09b07c75
rangeeeer <rezarangeeeer@gmail.com>
2022-07-04 21:33:18 +0430

    delete some of extra files

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,22 +2,24 @@ import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "Credentioals",
-      credentioals: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      async autorize(credentiols, req) {
-        const user = { id: 1, name: "J Smith", email: "jsmith@example.com" }
-        if (user) {
-          return user
-        } else {
-          return null
+export default async function auth(req, res) {
+  return await NextAuth(req, res, {
+    providers: [
+      CredentialsProvider({
+        name: "Credentioals",
+        credentioals: {
+          username: { label: "Username", type: "text" },
+          password: { label: "Password", type: "password" }
+        },
+        async autorize(credentiols, req) {
+          const user = { id: 1, name: "J Smith", email: "jsmith@example.com" }
+          if (user) {
+            return user
+          } else {
+            return null
+          }
         }
-      }
-    })
-  ]
-})
+      })
+    ]
+  })
+}
