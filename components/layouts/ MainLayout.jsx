d4c13171857a83b2207a086c1251e310583438b4
rangeeeer <rezarangeeeer@gmail.com>
2022-07-05 19:04:25 +0430

    make some changes to layouts

---
+++ components/layouts/ MainLayout.jsx
@@ -0,0 +1,8 @@
+export default function MainLayout(props) {
+  return (
+    <>
+      <div>layout</div>
+      {props.children}
+    </>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
-import styles from '../styles/Home.module.css'
+import MainLayout from "../components/layouts/ MainLayout.jsx"
 
 export default function Home() {
   return (
+    <MainLayout>
       <div className="container mx-auto bg-gray-200">
         <p>Hello World!</p>
       </div>
+    </MainLayout>
   )
 }
